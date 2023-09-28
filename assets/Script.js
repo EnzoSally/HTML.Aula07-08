941dd92b4085cea9aced7ebcdeafd4517dc2ee91
EnzoSally <135671514+EnzoSally@users.noreply.github.com>
2023-09-28 20:50:24 -0300

    botando funções

--- assets/Script.js
+++ assets/Script.js
@@ -1,3 +1,22 @@
+const tasks = [];
+
+function newId() {
+    return Math.floor(Math.random() * 1000);
+}
+
+function getTasks() {
+    return tasks;  
+}
+
 function createTask(taskTitle, taskDescription = "") {
+    let id = newId();
     
+    let task = {
+        id,
+        taskTitle: taskTitle, 
+        taskDescription: taskDescription,
+    }
+    
+    task.push(task);
+    return task;
 }
