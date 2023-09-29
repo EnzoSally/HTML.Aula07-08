3c234564e86a123f55223360a6d5cccb7e28ad30
EnzoSally <135671514+EnzoSally@users.noreply.github.com>
2023-09-28 21:27:47 -0300

    Fazendo função renderTasks

--- assets/Script.js
+++ assets/Script.js
@@ -1,4 +1,6 @@
 const tasks = [];
+const taskListElement = document.getElementById("taskListElement");
+const emptyTaskListElement = document.getElementById("emptyTaskListElement");
 
 function newId() {
     return Math.floor(Math.random() * 1000);
@@ -20,3 +22,19 @@ function createTask(taskTitle, taskDescription = "") {
     task.push(task);
     return task;
 }
+
+    function renderTasks(listElement = taskListElement, emptyMessage = emptyTaskListElement){
+
+        let finalHtml = "";
+
+        if (tasks.length === 0 ) {
+            emptyMessage.style.display = 'block';
+            listElement.style.display = 'none';
+            return;
+        }
+
+    emptyMessage.style.display = 'none';
+    listElement.style.display = 'flex';
+    }
+
+renderTasks();
